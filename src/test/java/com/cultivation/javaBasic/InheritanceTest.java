104c0117dd923641672b4bba2579b0b02874138b
lpdft <772346730@qq.com>
2020-08-10 09:23:56 +0800

    [ruyue] feat:should_use_caution_when_dealing_with_array_type test

--- src/test/java/com/cultivation/javaBasic/InheritanceTest.java
+++ src/test/java/com/cultivation/javaBasic/InheritanceTest.java
@@ -102,7 +102,7 @@ class InheritanceTest {
 
         // TODO: please modify the following code to pass the test
         // <--start
-        final Optional<Boolean> expected = Optional.empty();
+        final Optional<Boolean> expected = Optional.of(true);
         // --end-->
 
         assertEquals(expected.get(), willThrow);
