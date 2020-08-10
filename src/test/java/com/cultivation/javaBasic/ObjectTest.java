e9dd94dc9bddec054064dab233580214074dc0f3
lpdft <772346730@qq.com>
2020-08-10 10:21:48 +0800

    [ruyue] feat:should_get_message_of_var_length_parameters test

--- src/test/java/com/cultivation/javaBasic/ObjectTest.java
+++ src/test/java/com/cultivation/javaBasic/ObjectTest.java
@@ -172,7 +172,7 @@ class ObjectTest {
 
         // TODO: please modify the following code to pass the test
         // <--start
-        final String expected = null;
+        final String expected = "1\n2\n3\n";
         // --end-->
 
         assertEquals(expected, message);
